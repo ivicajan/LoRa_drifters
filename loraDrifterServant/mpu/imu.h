025571798a094afce339cd60daeba4c7114dec1a
Roarkey <roarke.holland@hotmail.com>
2021-11-30 15:28:09 +0800

    removed double up tinygps

--- loraDrifterServant/mpu/imu.h
+++ loraDrifterServant/mpu/imu.h
@@ -29,19 +29,10 @@ using namespace BLA;
 MPU9250 mpu;
 int sample_period = 50; //in ms
 float T_ = sample_period / 1000; //in Sec
-int Freq_acc = 1000 / sample_period; //40Hz
+// int Freq_acc = 1000 / sample_period; //40Hz
 char in123 = 'a';         // char for input debug
 char incomingByte = 'a';
 int count = 0;            //Count for drift velocity elimination
-
-// A sample NMEA stream.
-const char *gpsStream =
-  "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
-  "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
-  "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
-  "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
-  "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
-  "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
 
 // The TinyGPS++ object
 extern TinyGPSPlus gps;
@@ -55,7 +46,7 @@ float Pos[3][2] = {{}, {}};
 float Acc_bias[3] = {0.f, 0.f, 0.f};
 float Yaw[2] = {0.f, 0.f};
 float thread_G = 0.01f;
-int stationary = 1;
+// int stationary = 1;
 int count_S = 0, count_v = 0;
 
 float Lat_o, Lng_o;
@@ -85,8 +76,6 @@ float beta;
 BLA::Matrix<8, 8> big_zero;
 BLA::Matrix<8, 8> big_I;
 BLA::Matrix<8, 8, Diagonal<8, float>> big_diag;
-
-extern TinyGPSPlus gps;
 
 void Initial_Kalman() {
   //Form matrix reference.
@@ -163,8 +152,8 @@ void Update_Kalman() {
            X_INS(1) + T_ * U_INS(1),
            1 / 2 * T_ * X_INS(0) + X_INS(2),
            1 / 2 * T_ * X_INS(1) + X_INS(3),
-           X_INS(4) + T_ * U_INS(3)
-          };
+           X_INS(4) + T_ * U_INS(3)};
+
   P = A_E * P * ~A_E + B_E * Q * ~B_E + P_0 * beta;
 
 /*
@@ -213,8 +202,7 @@ void measure_gps_data() {
     //Get GPS measure in meter from ref point.
     Y_GPS = {get_diff_dist(Lat_o, gps.location.lat()),
              get_diff_dist(Lng_o, gps.location.lng()),
-             mpu.getYaw() / 180.f * PI
-            };
+             mpu.getYaw() / 180.f * PI};
   } else Serial.println(F("Location: INVALID"));
 #ifdef DEBUG_MODE
   Serial << "Lat: " <<  Y_GPS(0)
@@ -265,14 +253,14 @@ void initIMU() {
   //Detect if MPU setup correctly
   if(!mpu.setup(0x68)) {  // change to your own address
     while (1) {
-      Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+      Serial << "MPU connection failed. Please check your connection with `connection_check` example.\n";
       delay(1000);
     }
   } else Serial << "MPU is connected. \n";
 
   // MPU calibrate anytime you want to
-  Serial.println("Accel Gyro calibration will start in 5sec.");
-  Serial.println("Please leave the device still on the flat plane.");
+  Serial << "Accel Gyro calibration will start in 5sec.\n";
+  Serial << "Please leave the device still on the flat plane.\n";
   mpu.verbose(true);
   delay(5000);
   mpu.calibrateAccelGyro();
@@ -281,7 +269,17 @@ void initIMU() {
 
   //Initial Kalman and GPS
   Initial_Kalman(); //Initial parameters
-  while (*gpsStream) {
+
+  // A sample NMEA stream.
+  const char *gpsStream =
+    "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
+    "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
+    "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
+    "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
+    "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
+    "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
+
+  while(*gpsStream) {
     if(gps.encode(*gpsStream++))
       update_ref_location(); //Set first reference location
   }

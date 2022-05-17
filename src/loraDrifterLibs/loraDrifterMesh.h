629f26bd1c9b4c42a572eaeed9e1b80a9c3eaee7
Roarkeyboy <roarke.holland@hotmail.com>
2022-05-17 21:51:24 +0800

    added comments to the meshing code functions

--- src/loraDrifterLibs/loraDrifterMesh.h
+++ src/loraDrifterLibs/loraDrifterMesh.h
@@ -24,19 +24,69 @@ enum class MessageType: byte {
   Restart               = 0x46
 };
 
+/**
+ * @brief Send a packet to a master or servant node.
+ * 
+ * @param mode either servant or master mode who sent this packet
+ * @param type is the message type \ref MessageType that we are sending
+ * @param router is the device that is forwarding the packet
+ * @param recipient is who is intended to receive this packet
+ * @param sender is who sent this packet
+ * @param ttl time to live until packet is 'lost'
+ */
 void sendFrame(const int mode, const byte type, const byte router, const byte recipient, const byte sender, const byte ttl);
 
+/**
+ * @brief Listens for packets sent over LoRa and decodes them accordingly
+ * 
+ * @param frameSize size of the packet decoded
+ * @param mode either servant or master mode who sent this packet
+ * @return the status of the packet handling
+ */
 int listener(const int frameSize, const int mode);
 
+/**
+ * @brief Loop that checks if a certain amount of time has elapsed. Used for PL_TX_TIME definition time slot.
+ * 
+ * @param interval to check against
+ * @return true if past interval threshold else false
+ */
 bool runEvery(const unsigned long interval);
 
+/**
+ * @brief Loop that checks if a certain amount of time has elapsed. Used for RS_BCAST_TIME definition time slot.
+ * 
+ * @param interval to check against
+ * @return true if past interval threshold else false
+ */
 bool loop_runEvery(const unsigned long interval);
 
+/**
+ * @brief Convert the drifter index (i.e. 8) to the hex representation (i.e. 0x88)
+ * 
+ * @return the drifter ID
+ */
 byte indexToId(const int idx);
 
+/**
+ * @brief Send the positional packet payload to either master or to a forwarding node
+ * 
+ * @param mode either servant or master mode who sent this packet
+ * @param recipient is who is intended to receive this packet
+ * @param sender is who sent this packet
+ * @param ttl time to live until packet is 'lost'
+ * @param resend how many times we reattempt sending the packet
+ * 
+ * @return the status of the payload routing process
+ */
 int routePayload(const int mode, const byte recipient, const byte sender, const byte ttl, const int resend);
 
-// Send a broadcast to the network
+/**
+ * @brief Broadcast the routing status of the drifter to all other reachable nodes.
+ * 
+ * @param mode either servant or master mode who sent this packet
+ * @return the status of the broadcast
+ */
 int bcastRoutingStatus(const int mode);
 
 #endif // LORADRIFTERMESH_H
